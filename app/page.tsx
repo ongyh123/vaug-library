f12cbe3f6c5c6976e12b9af14297cc55281c58a7
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2024-04-24 23:53:05 +0800

    create main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-import Image from "next/image";
+"use client";
+
+import BookListingTable from "./components/BookListingTable";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Container from "./components/Container";
 
 export default function Home() {
-  return <div></div>;
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://fakerapi.it/api/v1/books")
+      .then((response) => {
+        setBooks(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <Container>
+      {books && (
+        <div className="relative w-full overflow-x-auto">
+          <BookListingTable books={books} />
+        </div>
+      )}
+    </Container>
+  );
 }
